94ace0dc2109c512ee93efcb9fbeafdff665e60c
AlanGrC22 <avillafan868@gmail.com>
2023-02-22 13:05:52 -0800

    task 1 fixed x1000000

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,13 +3,11 @@
 * main - void
 * Return: Always 0.
 **/
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
 	putchar('\n');
-
-	return (0);
 }
