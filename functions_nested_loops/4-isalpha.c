8e88f06cfc52d8a90eec679de4b463a7e5392aa9
AlanGrC22 <avillafan868@gmail.com>
2023-02-23 04:00:26 -0800

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - void
+ * @c: character
+ * Return: Always 0.
+**/
+int _isalpha(int c)
+{
+
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+		return (0);
+}
