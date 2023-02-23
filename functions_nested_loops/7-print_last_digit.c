718a3363a1999bc12160e983164ec20880bd40fb
AlanGrC22 <avillafan868@gmail.com>
2023-02-23 09:11:11 -0800

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_last_digit - imprime el ultimo digito de un numero
+* @n: digito a verificar
+* Return: Always 0.
+**/
+int print_last_digit(int n)
+{
+	putchar(n % 10 + '0');
+	return (0);
+}
