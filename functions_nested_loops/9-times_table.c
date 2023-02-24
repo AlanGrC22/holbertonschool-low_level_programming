96a1455236841e679be8bfd561f020c6e5ab8148
AlanGrC22 <avillafan868@gmail.com>
2023-02-24 11:43:55 -0800

    task 9 fixed x1000

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,27 +7,30 @@
 **/
 void times_table(void)
 {
-int a, b, c;
+	int a, b, c;
 
-for (a = 0; a <= 9; a++)
-{
-	for (b = 0; b <= 9; b++)
+	for (a = 0; a <= 9; a++)
 	{
-	c = a * b;
-	if (c > 9)
-	{
-	if ((c / 10) == 0)
-	{
-	_putchar(' ');
-	}
-	_putchar((c / 10) + '0');
-	}
-	_putchar((c % 10) + '0');
-	if (b < 9)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c > 9)
+		{
+			if ((c / 10) == 0)
+		{
+			_putchar(' ');
+		}
+			_putchar((c / 10) + '0');
+		}
+			_putchar((c % 10) + '0');
+			if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+			if (c <= 9)
+			_putchar(' ');
+		}
 	_putchar('\n');
 	}
 }
