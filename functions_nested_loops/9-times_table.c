266fec6aa1e6cf504f4b3abb858e7871b00ab06f
AlanGrC22 <avillafan868@gmail.com>
2023-02-24 09:51:19 -0800

    task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *times_table - tabla del 9
+ *@a, @b, @c: is a int
+**/
+void times_table(void)
+{
+int a, b, c;
+
+for (a = 0; a <= 9; a++)
+{
+	for (b = 0; b <= 9; b++)
+	{
+	c = a * b;
+	if (c > 9)
+	{
+		if ((c / 10) == 0)
+	{
+	_putchar(' ');
+	}
+	_putchar((c / 10) + '0');
+	}
+	_putchar((c % 10) + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	_putchar('\n');
+	}
+}
