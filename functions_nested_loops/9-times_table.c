96cb81ef69319c6e29976c598a576c49643319ab
AlanGrC22 <avillafan868@gmail.com>
2023-02-24 19:29:43 -0800

    task 9 fixeed

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,36 +1,38 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * times_table - tabla del 9
- * @a, @b, @c: is a int
- * return: times table
-**/
+ * times_table - prints the 9 times table, starting with 0
+ *
+ */
+
 void times_table(void)
 {
-	int a, b, c;
+	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0 ; a <= 9 ; ++a)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0 ; b <= 9 ; ++b)
 		{
-			c = a * b;
-			if (c > 9)
-		{
-			if ((c / 10) == 0)
-		{
-			_putchar(' ');
-		}
-			_putchar((c / 10) + '0');
-		}
-			_putchar((c % 10) + '0');
+			if ((a * b) >= 10)
+			{
+				_putchar(' ');
+				_putchar((a * b) / 10 + '0');
+				_putchar((a * b) % 10 + '0');
+			}
+			else if ((a * b) < 10 && b != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((a * b) + '0');
+			}
+			else
+				_putchar('0');
 			if (b < 9)
-		{
-			_putchar(',');
-			_putchar(' ');
+			{
+				_putchar(',');
+			}
+			else
+				_putchar('\n');
 		}
-			if (c <= 9)
-			_putchar(' ');
-		}
-	_putchar('\n');
 	}
 }
