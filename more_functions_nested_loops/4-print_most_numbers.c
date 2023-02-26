692eb7f4957cde541b794dcf68db882a583d054c
AlanGrC22 <avillafan868@gmail.com>
2023-02-26 07:28:50 -0800

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
-#include <stdbool.h>
 /**
  * print_most_numbers - prints the numbers, from 0 to 9, not print 2 and 4
- * @n: void n
+ * @n: not print
  * Return: Always 0.
 */
 void print_most_numbers(void)
 {
 		char i;
-		bool n = true;
 
 			for (i = 0; i <= 9; i++)
 			{
-			if (n && (i == 2 || i == 4))
+			if (i == 2 || i == 4)
 			{
 				continue;
 			}
