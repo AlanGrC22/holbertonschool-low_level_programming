438d2a5c04c439468ee670450f734f6ea1154a34
AlanGrC22 <avillafan868@gmail.com>
2023-02-26 06:56:10 -0800

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - void
+ * Return: Always 0.
+*/
+void print_numbers(void)
+{
+		char i;
+
+			for (i = 0; i <= 9; i++)
+			{
+				putchar(i + '0');
+			}
+				putchar('\n');
+}
