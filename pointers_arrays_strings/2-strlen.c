b75e00175da8201dc95ddf10b20ff54249a62fbb
AlanGrC22 <avillafan868@gmail.com>
2023-02-28 05:36:41 -0800

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - Write a function that returns the length of a string
+ * @s: string
+ * Return: Always 0
+*/
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
