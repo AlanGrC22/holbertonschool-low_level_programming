6a807ff84de78918993984c3005b1e6eb4d9daa3
AlanGrC22 <avillafan868@gmail.com>
2023-03-01 08:50:07 -0800

    task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ *_atoi - Write a function that convert a string to an integer
+ *@s: string
+ *Return: Always r
+ */
+int _atoi(char *s)
+{
+	int r = 0;
+
+		for (int i = 0; s[i] != '\0'; i++)
+		{
+		if ()
+		r = r * 10 + s[i] - '0';
+		}
+		while (r = r * 10 + s[i] - '0')
+
+	printf("\n");
+	return (r);
+}
