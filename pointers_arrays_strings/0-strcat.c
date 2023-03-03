e8ed3d1eb1b46aa1772c8d32f2e9825435fa2554
AlanGrC22 <avillafan868@gmail.com>
2023-03-03 05:28:14 -0800

    task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcat - Write a function that concatenates two strings
+* @dest: destino
+* @src: source
+* Return: dest
+**/
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	for (b = 0; dest[b] != '\0'; b++)
+	{
+		;
+	}
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[b + a] = src[a];
+	}
+	*dest = '\0';
+	return (dest);
+
+}
