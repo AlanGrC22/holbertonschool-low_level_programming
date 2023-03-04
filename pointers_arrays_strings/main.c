535057114086d3a497592270789c7e2d49fb2767
AlanGrC22 <avillafan868@gmail.com>
2023-03-04 11:22:10 -0800

    task 5

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+	    char str[] = "Look up!\n";
+	        char *ptr;
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+		    ptr = string_toupper(str);
+		        printf("%s", ptr);
+			    printf("%s", str);
+			        return (0);
 }
