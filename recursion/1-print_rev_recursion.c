0766b924eb3e120e4e6426063a1d384c6beda6b9
AlanGrC22 <avillafan868@gmail.com>
2023-03-06 05:35:53 -0800

    task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Write a function that prints a string in reverse.
+ *@s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	int l = 0;
+
+	for (l = 0; s[l]; l++)
+	{
+		;
+	}
+	for (l = l - 1; s[l]; l--)
+	{
+		putchar(s[l]);
+	}
+	putchar('\n');
+}
