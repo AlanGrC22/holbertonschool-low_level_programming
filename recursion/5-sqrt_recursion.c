7a16d44aac19c7b8563702c69a15bc83bed805dd
AlanGrC22 <avillafan868@gmail.com>
2023-03-10 13:37:30 -0800

    task 5 fixedd

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,38 +1,45 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt - returns the natural square root of a number
  * @n: number to return the square
+ * @e: end
+ * @s: start
  * Return: If n has a natural square root - the natural square root of n
  *         If n does not have a natural square root - -1
  */
 int _sqrt(int n, int s, int e);
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt(n, 1, n));
-}
-
-/**
- * _sqrt - return the square root of a number
- * @n: test number
- * @s: start
- * @e: end
- * Return: the square root of n
- */
-
 int _sqrt(int n, int s, int e)
 {
-	int m = (s + e) / 2;
-	int sqre = m * m;
-	if (s > e)
-		return (-1);
-	if (sqre == n)
-		return (m);
-	if (sqre > n)
-		return (_sqrt(n, s, m - 1));
-	else
-		return (_sqrt(n, m + 1, e));
+		int m = (s + e) / 2;
+
+		if (s > e)
+		{
+			return (-1);
+		}
+
+		if (m * m == n)
+		{
+			return (m);
+		}
+		if (m * m < n)
+		{
+			return (_sqrt(n, m + 1, e));
+		}
+		else
+		{
+			return (_sqrt(n, s, m - 1));
+		}
+}
+/**
+ * _sqrt_recursion - return the square root of a number
+ * @n: test number
+ * Return: the square root of n
+ */
+int _sqrt_recursion(int n)
+{
+		if (n == 0 || n == 1)
+		{
+			return (n);
+		}
+		return (_sqrt(n, 0, n));
 }
