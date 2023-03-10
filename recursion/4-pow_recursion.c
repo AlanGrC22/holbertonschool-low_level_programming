88a6af4e0bf559077d9b803808393d31651cecdf
AlanGrC22 <avillafan868@gmail.com>
2023-03-10 11:39:35 -0800

    task 4 fixed

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,12 +8,11 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (x < 0)
+	if (y < 0)
 	{
 	return (-1);
 	}
-	if (y <= 0)
-		return (0);
-	else
-		return (x * _pow_recursion(x, y - 1));
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
