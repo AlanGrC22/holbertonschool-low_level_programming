a8161e0e00eb1c468cbb4e61f388e425c55383ed
AlanGrC22 <avillafan868@gmail.com>
2023-03-12 18:58:14 -0700

    ]task 0

--- static_libraries/6-abs.c
+++ static_libraries/6-abs.c
@@ -1,11 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _abs - valor absoluto
- * @n: numero a convertir
- * Return: Always 0.
- **/
-int _abs(int n)
+* _abs - check the code
+* @c: integer
+* Return: Always c
+**/
+int _abs(int c)
 {
-	return (abs(n));
+	if (c < 0)
+	{
+		int abs_val;
+	abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
 }
