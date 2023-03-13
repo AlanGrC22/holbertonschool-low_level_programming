4fd50667b73b5b0ea0d8f13d44e4c4966606c6b1
AlanGrC22 <avillafan868@gmail.com>
2023-03-12 20:07:34 -0700

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Write a program that adds positive numbers
+ * @argc: number of arguments to the program
+ * @argv: array of pointers to the arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+
+	sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][a] != '\0' ; b++)
+		{
+			if (argv[a][b] < 47 || argv[a][b] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
