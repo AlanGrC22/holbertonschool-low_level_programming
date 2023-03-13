5b2398cae6d94fc3c62de603e218e5d6a5dd23b3
AlanGrC22 <avillafan868@gmail.com>
2023-03-12 19:46:14 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints its name, followed by a new line
+ * @argc: number of arguments typed
+ * @argv: array of pointers to the arguments
+ * Return: Always o.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
