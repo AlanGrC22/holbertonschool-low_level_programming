a864d86cd21f4676ebeca5d33e4cb126d775b85e
AlanGrC22 <avillafan868@gmail.com>
2023-06-23 05:52:54 -0700

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Write a function that adds two integers and returns the result.
+ * @one: integer one
+ * @two: integer two
+ * Return: result
+ **/
+int add(int one, int two)
+{
+	return (one + two);
+}
