4730fd736a9b102311b8d37a801d06aa5ddba10c
AlanGrC22 <avillafan868@gmail.com>
2023-06-23 05:25:46 -0700

    task 9 fixed

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - Write a function that prints the 9 times table
+ *               starting with 0.
+ **/
+void times_table(void)
+{
+	int l, c, n;
+
+	for (l = 0; l < 9; l++)
+	{
+		_putchar ('0');
+		_putchar (',');
+		_putchar (' ');
+		for (c = 0; c < 10; c++)
+		{
+			n = (l * c);
+			if ((n / 10) > 0)
+			{
+				_putchar ((n / 10) + '0');
+			}
+			else
+			{
+				_putchar (' ');
+			}
+			_putchar((n % 10) + '0');
+			if (c < 9)
+			{
+				_putchar (',');
+				_putchar (' ');
+			}
+		}
+			_putchar ('\n');
+	}
+}
