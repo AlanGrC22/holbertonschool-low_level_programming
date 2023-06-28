628598abbd9ddbe8e731dece7f8378aad477d2e1
AlanGrC22 <avillafan868@gmail.com>
2023-06-27 18:07:28 -0700

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ **/
+void rev_string(char *s)
+{
+	int count = 0;
+	int i, j;
+	char aux;
+
+	while (s[count] != '\0')
+		count++;
+	j = count -1;
+	for (i = 0; i < j; i++)
+	{
+		aux = s[i];
+		s[i] = s[j];
+		s[j] = aux;
+		j--;
+	}
+}
